1b7a5c33d72ec286a90a4bbd32203a28bb8682a7
ShaunCr <shaun.cromie@aol.com>
2019-10-07 18:57:46 +0200

    test

---
+++ src/main/java/TakeAway/AssesmentRest/App.java
@@ -0,0 +1,13 @@
+package TakeAway.AssesmentRest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
