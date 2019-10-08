601888f77ac21162dadc4bcf676871eae83e893a
ShaunCr <ShaunCr@MWLTPF1MEAFD.miway.co.za>
2019-10-08 18:00:42 +0200

    Created extra post test_post classes to experiment with

--- src/test/java/TakeAway/AssesmentRest/Test_Post2.java
+++ src/test/java/TakeAway/AssesmentRest/Test_Post2.java
@@ -5,7 +5,8 @@ import org.testng.annotations.Test;
 
 import com.github.cliftonlabs.json_simple.JsonObject;
 
-import io.restassured.RestAssured;
+import io.restassured.RestAssured.*;
+import io.restassured.matcher.RestAssuredMatchers.*;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -14,53 +15,7 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class Test_Post2 {
- // @Test
-  public void testIsReturningARequest() {
-	 // postJsonPayload();
-		/*
-	  RestAssured.baseURI ="https://jsonplaceholder.typicode.com/posts";
-	  RequestSpecification request = RestAssured.given();
-	  
-	  JsonObject requestParams = new JsonObject();
-	  requestParams.put("title","takeaway"); 
-	  requestParams.put("body","time to order food" );
-	  requestParams.put("userId", 2019);
-	  
-	// Add a header stating the Request body is a JSON
-	  request.header("Content-type", "application/json; charset=UTF-8");
-	   
-	  // Add the Json to the body of the request
-	  request.body(requestParams.toJson());
-	   
-	  // Post the request and check the response
-	  Response response = request.post();
-	  
-	  int statusCode = response.getStatusCode();
-	  Response r = response.peek();
-	  System.out.println(r);
-	  String statusCodeConverted = (String.valueOf(statusCode)).trim();
-	  
-	  System.out.println(statusCodeConverted);
-	  Assert.assertEquals(statusCodeConverted, "201");
-	  //String successCode = response.jsonPath().get("SuccessCode");
-	 // Assert.assertEquals( "Correct Success code was returned", successCode, "OPERATION_SUCCESS");
-*/
-	 /* 
-      RestAssured.baseURI = "https://jsonplaceholder.typicode.com/posts";
-      given().urlEncodingEnabled(true)
-          .param("title", "takeaway")
-          .param("body", "time to order food")
-          .param("userId", 2019)
-          .header("Content-Type", "application/json")
-          .post()
-          .then().statusCode(201);
-      
-  }
-  */
-	  
-	  
-	  
-  }	  
+  
   
   private static String payload = "{\n" +
 	        "  \"title\": \"takeaway\",\n" +
@@ -68,15 +23,30 @@ public class Test_Post2 {
 	        "  \"userId\": \"2019\"\n" +
 	        "}";
   	@Test
-	    public static void postJsonPayload() {
-	        
-            given()
-            .contentType(ContentType.JSON)
-            .body(payload).
-    	when()
-    		.post("https://jsonplaceholder.typicode.com/posts").
-	then().
-			assertThat().
-			statusCode(201);
+	    public static void postsTypicode() {
+  		
+  	//with().body(new O(5.25f, 1, 13.1f, "X"));        
+    given().log().all()
+	    .contentType(ContentType.JSON)
+	    .body(payload).
+	when()
+		.request("POST", "https://jsonplaceholder.typicode.com/posts").
+		//.post("https://jsonplaceholder.typicode.com/posts").
+	then().statusCode(201).
+		and().
+			body("title", equalTo("takeaway")).
+		and().
+			body("body", equalTo("time to order food")).
+		and().
+			body("userId",equalTo("2019")).
+		and().
+			body("id", equalTo(101))
+			.log().all()
+			;
+		
+
+
+            
+            
 }
 }
